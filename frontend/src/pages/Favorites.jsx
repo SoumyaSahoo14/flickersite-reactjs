2843a6d5807f288634416b9d66f49038bff5ca51
SoumyaSahoo14 <soumyajeet4321@gmail.com>
2025-06-13 11:20:34 +0530

    main

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,7 @@
+function Favorites(){
+    return<div className="favorites-empty">
+        <h2>No Favorite Pictures Yet</h2>
+        <p>Start adding pictures to your favorite and they will appear here</p>
+    </div>
+}
+export default Favorites;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,41 @@
 import PictureCard from "../Components/Picturecard"
-
+import { useState } from "react";
 function Home() {
+    const [searchQuery, setSearchQuery]= useState("");
 
     const pictures = [
         { id: 1, title: "Fast & Furious", release_date: "2022" },
         { id: 2, title: "Terminator", release_date: "2022" },
         { id: 3, title: "John wick", release_date: "2022" },
+    ];
+    const handleSearch =(e)=>{
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
 
+    };
 
-    ]
-    return <div className="home">
-        <div className="pictures-grid">
-            {pictures.map((picture) => (
-                <PictureCard picture={picture} key={picture.id} />
-                ))}
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text " 
+                placeholder="Search for pictures.." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e)=> setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            <div className="pictures-grid">
+                {pictures.map(
+                 (picture) => 
+                   picture.title.toLowerCase().startsWith(searchQuery)&&(
+                    <PictureCard picture={picture} key={picture.id} />
+                   )
+                )}
+            </div>
+
         </div>
-
-    </div>
+    );
 }
+export default Home;
